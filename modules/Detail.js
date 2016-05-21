f7b17b5150dec836905643672570bfc2bd94fa63
Mariah Tree <mariah.tree@gmail.com>
2016-05-21 17:54:14 -0500

    added font-awesome and placed characters on detail

--- modules/Detail.js
+++ modules/Detail.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router'
 
+
 export default React.createClass({
   render() {
     console.log(this.props);
     return (
       <div>
-      <h2><Link to="/">&#65513;</Link></h2>
+      <h2><Link to="/"><i className="fa fa-arrow-left"/></Link></h2>
         <ul>
-          <li>{this.props.params.name}</li>
-          <li>{this.props.params.email}</li>
-          <li>{this.props.params.phone}</li>
-          <li>{this.props.params.location}</li>
+          <li className="detail__li"><i className="fa fa-user"/>{this.props.params.name}</li>
+          <li className="detail__li"><i className="fa fa-envelope"/>{this.props.params.email}</li>
+          <li className="detail__li"><i className="fa fa-mobile"/>{this.props.params.phone}</li>
+          <li className="detail__li"><i className="fa fa-globe"/>{this.props.params.location}</li>
         </ul>
-      </div>
+
+        </div>
     )
   }
 })
